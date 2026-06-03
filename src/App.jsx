8b884e76cc281fecd2759dfbe8bd3d8c657572d8
AJIT MALLINATH AMANE <ajitamane@AJITs-MacBook-Pro.local>
2026-06-03 16:22:45 +0530

    Add API error handling messages

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,45 @@ function App() {
   const [state, setState] = useState("");
   const [city, setCity] = useState("");
 
+  const [countryError, setCountryError] = useState("");
+  const [stateError, setStateError] = useState("");
+
   useEffect(() => {
     fetch("https://location-selector.labs.crio.do/countries")
-      .then((res) => res.json())
-      .then((data) => setCountries(data))
-      .catch(() => setCountries([]));
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error();
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setCountries(data);
+        setCountryError("");
+      })
+      .catch(() => {
+        setCountries([]);
+        setCountryError("Unable to load countries");
+      });
   }, []);
 
   useEffect(() => {
     if (!country) return;
 
     fetch(`https://location-selector.labs.crio.do/country=${country}/states`)
-      .then((res) => res.json())
-      .then((data) => setStates(data))
-      .catch(() => setStates([]));
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error();
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setStates(data);
+        setStateError("");
+      })
+      .catch(() => {
+        setStates([]);
+        setStateError("Unable to load states");
+      });
   }, [country]);
 
   useEffect(() => {
@@ -31,13 +56,25 @@ function App() {
     fetch(
       `https://location-selector.labs.crio.do/country=${country}/state=${state}/cities`
     )
-      .then((res) => res.json())
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error();
+        }
+        return res.json();
+      })
       .then((data) => setCities(data))
-      .catch(() => setCities([]));
+      .catch(() => {
+        setCities([]);
+      });
   }, [country, state]);
 
   return (
     <div>
+      <h1>Select Location</h1>
+
+      {countryError && <p>{countryError}</p>}
+      {stateError && <p>{stateError}</p>}
+
       <select
         value={country}
         onChange={(e) => {
@@ -46,11 +83,16 @@ function App() {
           setCity("");
           setStates([]);
           setCities([]);
+          setStateError("");
         }}
       >
         <option value="">Select Country</option>
         {countries.map((countryName) => (
-          <option key={countryName} value={countryName}>
+          <option
+            key={countryName}
+            value={countryName}
+            data-testid={countryName}
+          >
             {countryName}
           </option>
         ))}
@@ -67,7 +109,11 @@ function App() {
       >
         <option value="">Select State</option>
         {states.map((stateName) => (
-          <option key={stateName} value={stateName}>
+          <option
+            key={stateName}
+            value={stateName}
+            data-testid={stateName}
+          >
             {stateName}
           </option>
         ))}
@@ -80,7 +126,11 @@ function App() {
       >
         <option value="">Select City</option>
         {cities.map((cityName) => (
-          <option key={cityName} value={cityName}>
+          <option
+            key={cityName}
+            value={cityName}
+            data-testid={cityName}
+          >
             {cityName}
           </option>
         ))}
